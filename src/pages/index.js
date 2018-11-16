159c949a6a6e706bf0411b0b369f118f427a1e88
Mariela Justiniano <marielajustiniano@MarielasMacBook.home>
2018-11-16 13:41:11 +0100

    little text changes

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <div  className="Hero">
+      <div className="HeroGroup">
+      
+    <h1>NYC Taxis Data</h1>
+    <p>This will be my data project about yellow and green taxis</p>
+    <Link to="/page-2/">See the data</Link>
+    </div>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
